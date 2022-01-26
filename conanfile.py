fb784be9af972ca3b2779995342d84a8d90c36e8
Perepelitsyn Stanislav <stas.perepel@gmail.com>
2022-01-26 23:30:39 +0200

    Header only cmake config

--- conanfile.py
+++ conanfile.py
@@ -15,8 +15,6 @@ class NanoIdXXConan(ConanFile):
     license = "MIT"
     
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = {"shared": False}
     generators = "cmake"
 
     def export_sources(self):
@@ -33,18 +31,11 @@ class NanoIdXXConan(ConanFile):
     def build(self):
         cmake = self.configure_cmake()
         cmake.build()
+        cmake.test()
 
     def package(self):
         # Copy headers to the include folder and libraries to the lib folder
-        self.copy("*.h", dst="include", src="src")
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.dylib*", dst="lib", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("*.h", dst="include", src="include")
 
-    def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
-        
     def package_id(self):
         self.info.header_only()
